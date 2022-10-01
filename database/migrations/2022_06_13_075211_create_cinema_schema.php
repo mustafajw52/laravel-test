f17decf74a6629b26328a0e7033ad6d254968db2
mustafajw52 <114802950+mustafajw52@users.noreply.github.com>
2022-10-01 16:23:39 +0530

    4th coding test changes

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -37,7 +37,88 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        Schema::create('movies', function($table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->dateTime('duration');
+            $table->string('language');
+            $table->dateTime('release_date');
+            $table->timestamps();
+        });
+
+        Schema::create('city', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('state');
+            $table->string('zipcode');
+            $table->timestamps();
+        });
+        
+        Schema::create('cinemas', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('total_cinema_halls');
+            $table->integer('city_id')->unsigned();
+            $table->foreign('city_id')->references('id')->on('city')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('cinema_halls', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('total_seats')->unsigned();
+            $table->integer('cinema_id')->unsigned();
+            $table->foreign('cinema_id')->references('id')->on('cinemas')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('cinema_seats', function($table) {
+            $table->increments('id');
+            $table->integer('seat_number');
+            $table->integer('type');
+            $table->integer('cinema_hall_id')->unsigned();
+            $table->foreign('cinema_hall_id')->references('id')->on('cinema_halls')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('shows', function($table) {
+            $table->increments('id');
+            $table->dateTime('date');
+            $table->dateTime('starttime');
+            $table->dateTime('endtime');
+            $table->integer('cinema_hall_id')->unsigned();
+            $table->foreign('cinema_hall_id')->references('id')->on('cinema_halls')->onDelete('cascade');
+            $table->integer('movie_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('bookings', function($table) {
+            $table->increments('id');
+            $table->integer('number_of_seats');
+            $table->dateTime('timestamp');
+            $table->integer('status');
+            $table->integer('show_id')->unsigned();
+            $table->foreign('show_id')->references('id')->on('shows')->onDelete('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('show_seats', function($table) {
+            $table->increments('id');
+            $table->integer('status');
+            $table->decimal('price', 9, 3);
+            $table->integer('cinema_seat_id')->unsigned();
+            $table->foreign('cinema_seat_id')->references('id')->on('cinema_seats')->onDelete('cascade');
+            $table->integer('show_id')->unsigned();
+            $table->foreign('show_id')->references('id')->on('shows')->onDelete('cascade');
+            $table->integer('booking_id')->unsigned();
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+            $table->timestamps();
+        });
+       
     }
 
     /**
